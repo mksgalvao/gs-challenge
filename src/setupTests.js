c32fbf9f627cd2e1c698f83918c23b3e139fc60a
mksgalvao <mksgalvao@gmail.com>
2020-12-23 23:32:56 +0100

    making contract tests work

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Issue: Error: Cross origin http://localhost forbidden
+// https://github.com/axios/axios/issues/2654
+import axios from "axios";
+axios.defaults.adapter = require("axios/lib/adapters/http");
